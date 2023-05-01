cbe3ce868912c5a63eba87423a69486d44f1ce7c
zkaki <zakakish@gmail.com>
2023-05-01 18:29:34 -0500

    updated controller and added main

--- controller.py
+++ controller.py
@@ -1,10 +1,23 @@
-class Television:
+from PyQt5.QtWidgets import *
+from view import *
+
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+class Controller:
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.power_button.clicked.connect(lambda: self.power())
+        self.mute_button.clicked.connect(lambda: self.mute())
+        self.channel_upbutton.clicked.connect(lambda: self.channel_up())
+        self.channel_downbutton.clicked.connect(lambda: self.channel_down())
+        self.vol_upbutton.clicked.connect(lambda: self.volume_up())
+        self.vol_downbutton.clicked.connect(lambda: self.volume_down())
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
@@ -19,15 +32,15 @@ class Television:
 
     def channel_up(self):
         if self.__status:
-            if self.__channel == Television.MAX_CHANNEL:
-                self.__channel = Television.MIN_CHANNEL
+            if self.__channel == Controller.MAX_CHANNEL:
+                self.__channel = Controller.MIN_CHANNEL
             else:
                 self.__channel += 1
 
     def channel_down(self):
         if self.__status:
-            if self.__channel == Television.MIN_CHANNEL:
-                self.__channel = Television.MAX_CHANNEL
+            if self.__channel == Controller.MIN_CHANNEL:
+                self.__channel = Controller.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
@@ -35,21 +48,32 @@ class Television:
         if self.__status:
             if self.__muted:
                 self.__muted = not self.__muted
-            if self.__volume < Television.MAX_VOLUME:
+            if self.__volume < Controller.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
         if self.__status:
             if self.__muted:
                 self.__muted = not self.__muted
-            if self.__volume > Television.MIN_VOLUME:
+            if self.__volume > Controller.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
-        if self.__muted:
-            return f'TV status: Power = {self.__status}, Channel = {self.__channel}, Volume = 0'
+    def output(self):
+        if self.__status:
+            self.status_text.setText('On')
+            if self.__muted:
+                self.mute_text.setText('Muted')
+                self.volume_text.setText('0')
+            else:
+                self.volume_text.setText(self.__volume)
+                self.mute_text.setText('Not Muted')
+            self.channel_text.setText(self.__channel)
         else:
-            return f'TV status: Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+            self.status_text.setText('Off')
+            self.volume_text.setText('')
+            self.mute_text.setText('')
+            self.channel_text.setText('')
+
 
 
 
