40ddedad51bf06f4405261303c113aaa90dd5aea
zkaki <zakakish@gmail.com>
2023-05-01 19:21:27 -0500

    finalized

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ from view import *
 
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
-class Controller:
+class Controller(QMainWindow, Ui_MainWindow):
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
@@ -20,15 +20,26 @@ class Controller:
         self.vol_downbutton.clicked.connect(lambda: self.volume_down())
         self.__status = False
         self.__muted = False
-        self.__volume = Television.MIN_VOLUME
-        self.__channel = Television.MIN_CHANNEL
+        self.__volume = Controller.MIN_VOLUME
+        self.__channel = Controller.MIN_CHANNEL
 
     def power(self):
         self.__status = not self.__status
-
+        if self.__status:
+            self.status_text.setText('On')
+        else:
+            self.status_text.setText('Off')
+            self.volume_text.setText('')
+            self.mute_text.setText('')
+            self.channel_text.setText('')
     def mute(self):
         if self.__status:
             self.__muted = not self.__muted
+            if self.__muted:
+                self.mute_text.setText('Muted')
+                self.volume_text.setText('0')
+            else:
+                self.mute_text.setText('Not Muted')
 
     def channel_up(self):
         if self.__status:
@@ -36,6 +47,8 @@ class Controller:
                 self.__channel = Controller.MIN_CHANNEL
             else:
                 self.__channel += 1
+                channel = str(self.__channel)
+                self.channel_text.setText(channel)
 
     def channel_down(self):
         if self.__status:
@@ -43,6 +56,8 @@ class Controller:
                 self.__channel = Controller.MAX_CHANNEL
             else:
                 self.__channel -= 1
+                channel = str(self.__channel)
+                self.channel_text.setText(channel)
 
     def volume_up(self):
         if self.__status:
@@ -50,6 +65,8 @@ class Controller:
                 self.__muted = not self.__muted
             if self.__volume < Controller.MAX_VOLUME:
                 self.__volume += 1
+                volume = str(self.__volume)
+                self.volume_text.setText(volume)
 
     def volume_down(self):
         if self.__status:
@@ -57,6 +74,8 @@ class Controller:
                 self.__muted = not self.__muted
             if self.__volume > Controller.MIN_VOLUME:
                 self.__volume -= 1
+                volume = str(self.__volume)
+                self.volume_text.setText(volume)
 
     def output(self):
         if self.__status:
@@ -73,7 +92,6 @@ class Controller:
             self.volume_text.setText('')
             self.mute_text.setText('')
             self.channel_text.setText('')
-
 
 
 
